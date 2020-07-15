49532b22c9ebe502af59916b3c4cfa029c8d6634
Parkduksung <duksung1234@naver.com>
2020-07-16 04:39:03 +0900

    naver search range 범위 설정, search display 설정

--- app/src/main/java/com/sample/rxnaversearchapi/network/api/MovieSearchApi.kt
+++ app/src/main/java/com/sample/rxnaversearchapi/network/api/MovieSearchApi.kt
@@ -4,12 +4,20 @@ import com.sample.rxnaversearchapi.network.model.MovieDataResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.*
 
 interface MovieSearchApi {
     @GET("v1/search/movie.json")
     fun getMovieList(
-        @Query("query") keyWord: String
+        @Query("query") keyWord: String,
+        @Query("display") display: Int = INIT_DISPLAY,
+        @Query("yearfrom") searchRangeYearStart: Int = START_YEAR,
+        @Query("yearto") searchRangeYearEnd: Int = END_YEAR
     ): Call<MovieDataResponse>
 
-
+    companion object {
+        private const val INIT_DISPLAY = 100
+        private const val START_YEAR = 2000
+        private var END_YEAR = Calendar.getInstance().get(Calendar.YEAR)
+    }
 }
